a2a7f128b5785b5f9268a7eb7b41833f999f1770
mk82058104 <95918684+mk82058104@users.noreply.github.com>
2022-07-28 08:33:05 +0530

    Update FirstP.java

--- AmazonTest/src/com/amazontc/FirstP.java
+++ AmazonTest/src/com/amazontc/FirstP.java
@@ -3,7 +3,8 @@ package com.amazontc;
 public class FirstP {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("First Line of code");
+		System.out.println("2nd  Line of code");
 
 	}
 
